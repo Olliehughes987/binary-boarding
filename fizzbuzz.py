8d58f2f69bf9e07e35bb67541dc01b180f6c5624
Olliehughes987 <60144753+Olliehughes987@users.noreply.github.com>
2021-08-24 00:29:44 +0100

    Fixed formatting issue

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
-for i in range(1,100):
-    if(i % 15 == 0) :
+for i in range(1,101):
+    if i % 15 == 0 :
       print("FizzBuzz")
-    elif (i % 5 == 0):
+    elif i % 5 == 0:
       print("Buzz")
-    elif (i % 3 == 0):
+    elif i % 3 == 0:
       print("Fizz")
     else:
       print(i)
