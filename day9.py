de60028f580c238dbefc88569666764f5aca4747
Olliehughes987 <60144753+Olliehughes987@users.noreply.github.com>
2021-08-25 16:57:08 +0100

    Update day9.py

--- day9.py
+++ day9.py
@@ -1,37 +1,54 @@
-   
-def calculateNonSum(curr, preamble):
+# --- Day 9: Encoding Error --- #
+
+def calculateNonValidNum(curr, preamble):
     start = 0
-    sub = curr[start:preamble]
+    sect = curr[start:preamble]
+    # iterates through array after preamble, checking each value
+    # against ones within the section defined by the preamble
     for target in curr[preamble:]:
         found = False
+        
         for i in curr[:preamble]:
+            # calculates the number needed for the current number to be valid
             y = target - i
-            if str(y) in sub:
+
+            #checks if that number is in the current section of the array
+            if str(y) in sect:
+                # increases the start and preamble index by 1, so that the section moves forward 1 index in the array
                 start += 1
                 preamble += 1
-                sub = curr[start:preamble]
+                sect = curr[start:preamble]
                 found = True
                 break
+            
+        # If no valid combination is found, it prints the number and breaks out of the loop.
         if found == False:
             print("First non valid number:",target)
-            targInd = curr.index(target)
             break
 
 
 
 def calculateSums(arr, targ, ind):
+    # creates the section with first 2 elements inside.
     start = 0
     length = 2
-    slice = arr[start:length]
-    while sum(slice) != 217430975:
-        slice = arr[start:length]
-        if(sum(slice) > 217430975):
+    sect = arr[start:length]
+    
+    # while the sum of the section elements is not the target value,
+    # increase the length of the section by adding more elements from the list.
+    while sum(sect) != 217430975:
+        sect = arr[start:length]
+        # once the sum of the section is larger than the target, reset the length to 2
+        # and increase the start index by 1.
+        if(sum(sect) > 217430975):
             start += 1
             length = 2
+            
         length += 1
-    print("Contiguous values:",slice)
-    slice.sort()
-    print("Total of smallest and biggest element:",slice[0] + slice[-1])
+    # prints the contiguous values and the total of the largest and smallest element combined.
+    print("Contiguous values:",sect)
+    sect.sort()
+    print("Total of smallest and biggest element:",sect[0] + sect[-1])
         
 
 
@@ -45,5 +62,5 @@ inputList = list(map(int, inputList))
 
 
 
-calculateNonSum(inputList, 25)
+calculateNonValidNum(inputList, 25)
 calculateSums(inputList, 217430975, inputList.index(217430975))
